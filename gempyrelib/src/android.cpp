db01b313cc13b61ae2c17c2a7ec963ee45d65405
Markus Mertama <mertama@gmail.com>
2020-08-04 22:48:08 +0300

    test android

--- gempyrelib/src/android.cpp
+++ gempyrelib/src/android.cpp
@@ -15,11 +15,16 @@ Java_com_gempyre_myapp_MainActivity_callMain
     Androidobj = obj;
     return main(0, nullptr);
 }
+
+JNIEXPORT jint JNICALL
+JNI_OnLoad_gempyre(JavaVM*, void*) {
+    return JNI_VERSION_1_8;
+}
     
 int androidLoadUi(const std::string& url) {
     jstring urlString = Androidenv->NewStringUTF(url.c_str());
     jclass cls = Androidenv->GetObjectClass(Androidobj);
-    jmethodID methodId = Androidenv->GetMethodID(cls, "onLoad", "([Ljava/lang/String;)I");
+    jmethodID methodId = Androidenv->GetMethodID(cls, "onUiLoad", "([Ljava/lang/String;)I");
     if (methodId == 0) {
         return -99;
     }

eeb4bd9578254f79b1e4b873609b530cacdcaf49
Markus Mertama <mertama@gmail.com>
2021-04-11 23:58:41 +0300

    install both debug and release versions

--- msvc_install.bat
+++ msvc_install.bat
@@ -5,16 +5,27 @@ echo "msvc_install.bat <DIR>"
 echo DIR is optional and points to the install dir, defaults defined in GnuInstallDirs where the find_package should find it.
 goto exit
 :pass_ver
-if not exist "build" mkdir build
-pushd build
-set PREFIX=
 if not "%1"=="" set PREFIX=-DCMAKE_INSTALL_PREFIX=%1
 if "%1"=="" set PREFIX=-UCMAKE_INSTALL_PREFIX
+set PREFIX=
+
+if not exist "build" mkdir build
+
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DHAS_AFFILIATES=OFF -DHAS_TEST=OFF -DHAS_EXAMPLES=OFF %PREFIX%
+cmake --build . --config Debug
+set BUILD_PATH=%CD%
+popd
+echo Start an elevated prompt for an install.
+powershell -Command "Start-Process scripts\win_inst.bat -Verb RunAs -ArgumentList "%BUILD_PATH%,Debug"
+
+pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DHAS_AFFILIATES=OFF -DHAS_TEST=OFF -DHAS_EXAMPLES=OFF %PREFIX%
 cmake --build . --config Release
 set BUILD_PATH=%CD%
 popd
 echo Start an elevated prompt for an install.
-powershell -Command "Start-Process scripts\win_inst.bat -ArgumentList %BUILD_PATH% -Verb RunAs"
+powershell -Command "Start-Process scripts\win_inst.bat -Verb RunAs -ArgumentList %BUILD_PATH%"
+
 echo done
 :exit
--- scripts/win_inst.bat
+++ scripts/win_inst.bat
@@ -1,6 +1,8 @@
-@echo off
+rem @echo off
 echo This file is run in elevated mode due it will install libaries into system.
 echo The location is documented along GnuInstallDirs.
+set PARAM=
+if "%2"=="Debug" set PARAM=--config Debug
 cd %1
-cmake --install .
+cmake --install . %PARAM%
 
